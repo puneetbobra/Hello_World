2c5cb4943e74f424914fac6f6514acd23853d222
puneetbobra <bobrapuneet@yahoo.co.in>
2021-05-25 21:59:40 -0700

    test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to IS-657 from Puneet Bobra</Text>
       <StatusBar style="auto" />
     </View>
   );
